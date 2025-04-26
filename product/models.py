b7646fe964eabef2a09bdbe76f1f2a7944430bf4
Vijaylaxmi Hanamshetty <vijaylaxmihanamshetty38@gmail.com>
2025-04-26 17:54:58 +0530

    Add product app in our project with models

---
+++ product/models.py
@@ -0,0 +1,29 @@
+# product/models.py
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    stock = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image_url = models.URLField()
+    is_primary = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{'Primary' if self.is_primary else 'Secondary'} Image for {self.product.name}"
